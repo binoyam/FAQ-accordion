fdde0277ecc7a601d3b212575628a5d225fe2499
binoyam <binyam.techan01@gmail.com>
2023-12-10 04:48:35 +0300

    js improved

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ function showAnswer() {
   var answer = this.nextElementSibling;
   var icon = this.querySelector(".icon");
   var iconImg = icon.querySelector("img");
-  if (answer.style.display === "block") {
+  if (answer.classList.contains('active')) {
+    answer.classList.remove("active");
     answer.style.display = "none";
     iconImg.src = "./images/icon-plus.svg";
   } else {
+    answer.classList.add("active");
     answer.style.display = "block";
     iconImg.src = "./images/icon-minus.svg";
   }
 }
-//can be better
